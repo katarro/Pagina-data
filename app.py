1f0c97ab64d5b45a5d768ea7dac4381922191422
katarro <felipe.castro3@mail.udp.cl>
2024-06-24 23:07:17 -0400

    feature(add): Se añadieron estilos nuevos

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
-from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-Bootstrap(app)
-
-# Simulación de tu algoritmo de inteligencia artificial
-def get_best_property(criteria):
-    # Aquí iría la lógica de tu algoritmo de inteligencia artificial
-    # Por ahora, solo retornaremos un ejemplo de propiedad
-    return {
-        "address": "Calle Falsa 123, Santiago Centro",
-        "price": "100,000,000 CLP",
-        "description": "Un hermoso apartamento en el corazón de Santiago Centro.",
-        # "image_url": "https://via.placeholder.com/150"
-    }
 
 @app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/search', methods=['POST'])
-def search():
-    criteria = request.form['criteria']
-    best_property = get_best_property(criteria)
-    return render_template('index.html', property=best_property)
+
+def home():
+    image_url = url_for('static', filename='img/slide-2.jpg')
+    return render_template('index.html', image_url=image_url)
+
+@app.route('/buscar_propiedad', methods=['POST'])
+def buscar_propiedad():
+    # Aquí puedes obtener los datos del formulario
+    keyword = request.form.get('keyword')
+    print(keyword)
+
+    # Lógica para buscar el departamento
+    departamento = buscar_departamento(keyword)
+
+    # Puedes redirigir a una página de resultados o renderizar una plantilla con los resultados
+    return render_template('resultados.html', departamento=departamento)
+
+def buscar_departamento(keyword):
+    # Ejemplo de datos de departamento
+    departamento = {
+        'nombre': 'Departamento de Lujo',
+        'ubicacion': 'Santiago',
+        'precio': '200.000.000 CLP',
+        'descripcion': 'Un hermoso departamento en el centro de Santiago.'
+    }
+    return departamento
 
 if __name__ == '__main__':
     app.run(debug=True)
